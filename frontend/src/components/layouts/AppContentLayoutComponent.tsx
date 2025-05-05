23c1f90320b66f0e3aac46f22aabfd7d6424cf72
jonasans2 <jonasans2@live.com.ar>
2025-05-05 14:42:10 -0300

    jS

--- frontend/src/components/layouts/AppContentLayoutComponent.tsx
+++ frontend/src/components/layouts/AppContentLayoutComponent.tsx
@@ -71,7 +71,7 @@ export default function AppContentLayoutComponent(
                     <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                       <CgProfile className="text-2xl text-gray-600 group-hover:text-white" />
                       <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                        Internos
+                        Clientes
                       </h3>
                     </div>
                   </Link>
